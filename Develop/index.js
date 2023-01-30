e691bd38c1d07feb4248e7e4bd5e1c4fb1fba5bb
Kyle Hipple <76081114+KlyDesign@users.noreply.github.com>
2023-01-30 13:44:08 -0600

    questoins move around for readme gen

--- Develop/index.js
+++ Develop/index.js
@@ -1,46 +1,138 @@
-// TODO: Include packages needed for this application
+// a high-quality, professional README.md is generated with the title of my project and 
+// sections.....
+  //Title
+  // Description, 
+  // Table of Contents, 
+  // Installation, 
+  // Usage, 
+  // Contributing, 
+  // Tests, 
+  // License, 
+  // Questions(Link to github and email for additional questions)
+
+
 import inquirer from 'inquirer';
 import fs from 'fs';
+const {generateMarkdown} = ('../utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
 const questions = [
-        'What is you title',
-        'What is your github Profile Link'
+  {
+  type: 'input',
+  message: 'Project Title: ',
+  name: 'title'
+  },
+  {
+  type: 'input',
+  message: 'Description of the Project: ',
+  name: 'descript',
+  },
+  {
+  type: 'input',
+  message: 'Installation Instructions: ',
+  name: 'install',
+  },
+  {
+  type: 'input',
+  message: 'How to Use Application: ',
+  name: 'use',
+
+  },
+  {
+  type: 'input',
+  message: 'Contributions: ',
+  name: 'contri',
+  },
+  {
+  type: 'input',
+  message: 'Tests: ',
+  name: 'tests',
+  },
+  {
+    type: 'list',
+    message: 'Liscense on Repo: ',
+    name: 'liscense',
+    choices: ["The MIT License", "GNU License" , "Apache License" , "N/A"]
+
+  },
+  {
+  type: 'input',
+  message: 'What is your github username: ',
+  name: 'user',
+  },
+  {
+  type: 'input',
+  message: 'What is your email address project related questions: ',
+  name: 'email',
+
+  }
 ];
 
-inquirer.prompt(
-    [
-    {
-      type: 'input',
-      message: questions[0],
-      name: 'Title',
-    },
-    {
-      type: 'input',
-      message: questions[1],
-      name: 'password',
-    },
-    {
-      type: 'input',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-  ])
-  .then((response) =>
-    // response.confirm === response.password
-      console.log(response)
-  );
+
+questions()
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) 
 function writeToFiler(response) {
-    fs.writeFile('log.txt', `Title = ${Title}\n`, (err) =>
+    fs.writeFile('./generatedREADME.md', response, err =>
     err ? console.error(err) : console.log('Wrote File!')
 );
 }
 
 // TODO: Create a function to initialize app
-function init() {
+function init(questions) {
+  inquirer.prompt(
+    [
+    {
+      type: 'input',
+      message: questions[0],
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: questions[1],
+      name: 'descript',
+    },
+    {
+      type: 'input',
+      message: questions[2],
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: questions[3],
+      name: 'use',
+    },
+    {
+      type: 'input',
+      message: questions[4],
+      name: 'contri',
+    },
+    {
+      type: 'input',
+      message: questions[5],
+      name: 'tests',
+    },
+    {
+      type: 'list',
+      message: questions[6],
+      name: 'liscense',
+      choices: ["The MIT License", "GNU License" , "Apache License" , "N/A"]
+    },
+    {
+      type: 'input',
+      message: questions[7],
+      name: 'user',
+    },
+    {
+      type: 'input',
+      message: questions[8],
+      name: 'email',
+    },
+  ])
+  .then((response) =>
+      console.log(response)
+  );
 }
 
 // Function call to initialize app
 init();
+module.exports()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const index = require('../index.js')
+function renderLicenseBadge(license) {
+  let badge = '';
+  if(license === 'The MIT License'){
+    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }else if(license === 'GNU License'){
+    badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  else {
+    badge = '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = '';
+  if(license === 'The MIT License'){
+    link = 'https://choosealicense.com/licenses/mit/'
+  }else if(license === 'GNU License'){
+    link = 'https://choosealicense.com/licenses/mit/';
+  }
+  else {
+    link = 'https://choosealicense.com/licenses/mit/';
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ##${renderLicenseLink(data.license)} ${renderLicenseBadge(data.license)}
+  ##${renderLicenseLink(data.license)}
+
+  ## Table of Contents: 
+  ### * [Liscense](#lisences)
 `;
 }
 
